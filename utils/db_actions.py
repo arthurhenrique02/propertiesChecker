89e72ba8d43ccfbc813519c4e2474bf7b77ca1e2
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-12-15 18:18:02 -0300

    renomeado modulo

--- utils/db_actions.py
+++ utils/db_actions.py
@@ -1,11 +1,28 @@
 import datetime
 from typing import List
 
-from main import mongo
+from celery import shared_task
+
+
+@shared_task
+def add_new_property(data: dict, msg: str = "") -> None:
+    """Add new property to database."""
+    # import locally
+    from main import mongo
+
+    # check if property already exists
+    if mongo.db.properties_checker.find_one({"N° do imóvel": data["N° do imóvel"]}):
+        return
+
+    # add message field and insert
+    data["msg"] += f"{msg}\n"
+    mongo.db.properties_checker.insert_one(data)
 
 
 def save_changes_on_db(data: dict, msg: List[str] = None) -> None:
     """Save on database."""
+    # import locally
+    from main import mongo
 
     # check if data already exists
     if mongo.db.properties_checker.find_one({"N° do imóvel": data["N° do imóvel"]}):
@@ -40,6 +57,13 @@ def save_changes_on_db(data: dict, msg: List[str] = None) -> None:
         return
 
     print("Inserting...")
-    # add message field and insert by default
-    data["msg"] = ""
-    mongo.db.properties_checker.insert_one(data)
+    # add property
+    add_new_property.delay(data)
+
+
+def get_all_properties() -> List[dict]:
+    """Get all properties from database."""
+    # import locally
+    from main import mongo
+
+    return list(mongo.db.properties_checker.find())
